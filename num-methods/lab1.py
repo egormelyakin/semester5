e2b261d29ae95b49522129abb9cfadfbab8cc17d
egormelyakin <foodvegan5@gmail.com>
2022-09-01 11:44:58 +0300

    Update lab1.py

--- num-methods/lab1.py
+++ num-methods/lab1.py
@@ -1,51 +1,31 @@
-def lagrange_interpolation(x, values):
+import math
+
+def lagrange_interpolation(x, y, x0):
     result = 0.0
-    for i in range(len(values)):
-        L = 1.0
-        for j in range(len(values)):
+    for i in range(len(x)):
+        p = 1.0
+        for j in range(len(x)):
             if i != j:
-                L *= (x - values[j][0]) / (values[i][0] - values[j][0])
-        result += values[i][1] * L
+                p *= (x0 - x[j]) / (x[i] - x[j])
+        result += y[i] * p
     return result
 
-def newton_interpolation(x, values):
-    result = 0.0
-    for i in range(len(values)):
-        N = 1.0
-        for j in range(len(values)):
-            if i != j:
-                N *= (x - values[j][0])
-        result += values[i][1] / N
-    return result
+def newton_interpolation(x, y, x0):
+    pass
 
-def aitken_interpolation(x, values):
-    result = 0.0
-    for i in range(len(values)):
-        A = 1.0
-        for j in range(len(values)):
-            if i != j:
-                A *= (x - values[j][0]) / (values[i][0] - values[j][0])
-        result += values[i][1] * A
-    return result 
+def aitken_scheme(x, y, x0, eps):
+    pass
 
 def main():
-    x = 1.43
-    values = [
-        (1.0, 2.7182),
-        (1.1, 3.0041),
-        (1.2, 3.3201),
-        (1.3, 3.6692),
-        (1.4, 4.0552),
-        (1.5, 4.4816),
-        (1.6, 4.9530),
-        (1.7, 5.4739),
-        (1.8, 6.0496),
-        (1.9, 6.6858),
-        (2.0, 7.3890)
-    ]
-    print("Lagrange interpolation:", lagrange_interpolation(x, values))
-    print("Newton interpolation:", newton_interpolation(x, values))
-    print("Aitken interpolation:", aitken_interpolation(x, values))
+    x = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
+    y = [2.7182, 3.0041, 3.3201, 3.6692, 4.0552, 4.4816, 4.9530, 5.4739, 6.0496, 6.6858, 7.3890]
+    x0 = 1.43
+    eps = 10e-3
 
+    print(f'Многочлен Лагранжа: f({x0}) = {"%.4f" % lagrange_interpolation(x, y, x0)}')
+    print(f'Многочлен Ньютона:  f({x0}) = {"%.4f" % newton_interpolation(x, y, x0)}')
+    print(f'Схема Айткена:      f({x0}) = {"%.4f" % aitken_scheme(x, y, x0, eps)}')
+    print(f'Точное значение:    f({x0}) = {"%.4f" % math.exp(x0)}')
+    
 if __name__ == "__main__":
     main()
